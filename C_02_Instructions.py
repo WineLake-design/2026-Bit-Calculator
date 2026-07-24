286b69f2c58b28c4bbfbe3f57e78bb597bd53480
WineLake-design <tusamitchells50184@masseyhigh.school.nz>
2026-07-24 14:22:28 +1200

    Working Instructions and File Type checker

---
+++ C_02_Instructions.py
@@ -0,0 +1,25 @@
+# generates headings (eg:---- heading ----)
+
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+# Displays instructions
+def instructions():
+    statement_generator("Instructions","-")
+
+    print('''
+Instructions go here.
+- instruction 1 
+- instruction 2
+- etc
+    ''')
+
+
+# Main routine goes here
+want_instructions = input("Press <enter> to read the instructions "
+                          "or any key to continue ")
+
+if want_instructions == "":
+    instructions()
+
+print("program continues")
